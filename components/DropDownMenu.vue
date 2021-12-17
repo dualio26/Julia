18069685b8b0210d5def94bb2726cc8e0235a840
dualio26 <eamongreen59@gmail.com>
2021-12-17 23:06:01 +1300

    icons for phone nav

--- components/DropDownMenu.vue
+++ components/DropDownMenu.vue
@@ -1,3 +1,14 @@
 <template>
-  <p>he;[</p>
+  <div class="grid grid-cols-6 place-content-center">
+    <i class="fas fa-home mx-auto fa-lg"></i
+    ><i class="fas fa-user mx-auto fa-lg"></i
+    ><i class="far fa-image mx-auto fa-lg"></i
+    ><i class="fas fa-palette mx-auto fa-lg"></i
+    ><i class="fab fa-blogger mx-auto fa-lg"></i
+    ><i class="fas fa-sms mx-auto fa-lg"></i>
+  </div>
 </template>
+
+<style scoped>
+@import 'https://pro.fontawesome.com/releases/v5.10.0/css/all.css';
+</style>
